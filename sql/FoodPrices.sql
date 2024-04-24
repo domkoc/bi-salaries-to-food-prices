4fdcfd091da77c66c859dd03da1bf892c3ac5e1d
Kocka Dominik <kocka98@gmail.com>
2024-04-24 18:52:14 +0200

    Add basic import of salaries

---
+++ sql/FoodPrices.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "FoodPrices" (
+     "Country" nvarchar(1000),
+     "Region" nvarchar(1000),
+     "Market" nvarchar(1000),
+     "Commodity" nvarchar(1000),
+     "Price" float,
+     "Unit" nvarchar(1000)
+ )
---
+++ sql/Salaries.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "Salaries" (
+     "CountryCode" nvarchar(1000),
+     "CountryName" nvarchar(1000),
+     "Year" nvarchar(1000),
+     "Value" float
+ )
